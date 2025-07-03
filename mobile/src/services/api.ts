e02ece7f6ef2cc9b5b6401a7bb916dd0dd15e869
Rafael Conrado de Souza <156972984+rafconrado@users.noreply.github.com>
2025-07-03 18:02:10 -0300

    atualizacao

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,21 +1,40 @@
 import { AppError } from "@utils/AppError";
 
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
+
+type SignOut = () => void;
+
+type APIIntanceProps = AxiosInstance & {
+  registerInterceptTokenManager: (SignOut: SignOut) => () => void
+}
 
 const api = axios.create({
-  baseURL: "http://192.168.1.16:3333",
-  timeout: 15000,
-});
+  baseURL: "http://192.168.1.23:3333"
+}) as APIIntanceProps;
 
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response && error.response.data) {
-      return Promise.reject(new AppError(error.response.data.message));
-    } else {
-      return Promise.reject(error);
+api.registerInterceptTokenManager = SignOut => {
+  const interceptTokenManager = api.interceptors.response.use(
+    (response) => response, requestError => {
+
+      if (requestError?.response?.status === 401) {
+        if (requestError.response.data?.message === "token.expired" || requestError.response.data?.message === 'token.invalid') {
+          
+        }
+        SignOut()
+      }
+
+      if (requestError.response && requestError.response.data) {
+        return Promise.reject(new AppError(requestError.response.data.message));
+      } else {
+        return Promise.reject(requestError);
+      }
+
     }
-  }
-);
+  );
+
+  return () => {
+    api.interceptors.response.eject(interceptTokenManager);
+  };
+};
 
 export { api };
